ba9ca245052427fb8dde78f1b8e071f7d2b54e92
thezaza101 <thezaza102@gmail.com>
2018-05-21 09:03:33 +1000

    Testing

--- Startup.cs
+++ Startup.cs
@@ -26,8 +26,31 @@ namespace DXAUpdater
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            string dbConn = "";
+            try
+            {
+                dbConn = Environment.GetEnvironmentVariable("DATABASE_URL").Trim();
+            }
+            catch
+            {
+                System.Console.WriteLine("Error getting DATABASE_URL");
+            }
+            if (string.IsNullOrEmpty(dbConn))
+            {
             services.AddDbContext<UpdatedDataContext>(options =>
                 options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
+            }
+            else
+            {
+                string host = dbConn.Substring(dbConn.IndexOf(@"@"),dbConn.LastIndexOf(@":"));
+                string user = dbConn.Substring(dbConn.IndexOf(@"/"),dbConn.IndexOf(@":"));
+                string password = dbConn.Substring(dbConn.IndexOf(@"//"),dbConn.IndexOf(@"@"));
+                string database = dbConn.Substring(dbConn.LastIndexOf(@"/"),dbConn.Length);
+                string databaseConnectionString = "Host="+host+";Database="+database+";Username="+user+";Password="+password+";";
+                System.Console.WriteLine(databaseConnectionString);
+                services.AddDbContext<UpdatedDataContext>(options =>
+                options.UseNpgsql(databaseConnectionString));
+            }
                 
             services.AddMvc();
         }
